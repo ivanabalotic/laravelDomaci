91f19c44ee7c85364770a9266ef350d8b55c021b
nzmnisam <milostaptap@gmail.com>
2021-02-28 21:14:54 +0100

    Nesto nesto nesto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('Blogs for app devs', 'Blogs for app devs') }}</title>
 
     <!-- Scripts -->
+    <script>
+
+    </script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src = "{{ asset('js/js.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -23,8 +27,13 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                @guest
+                    @if (Route::has('home'))
+                <a class="navbar-brand" href="{{ route('webLogin') }}">
+                    @endif
+                @endguest
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    {{ config('Articles for app devs', 'Articles for app devs') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -39,37 +48,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="nav-item">
+                                <a href="{{ route('writeArticle') }} " class="nav-link" > Write an article</a>
                             </li>
-                        @endguest
                     </ul>
                 </div>
             </div>
@@ -81,3 +62,17 @@
     </div>
 </body>
 </html>
+<script>
+function openArticlePage(id) {
+    console.log(id);
+    let url = "{{ route('viewArticle', ':id') }}";
+    url = url.replace(':id', id);
+    document.location.href = url;
+}
+
+function submitArticle() {
+    let url = "{{ route('viewArticle', ':id') }}";
+    url = url.replace(':id', id);
+    document.location.href = url;
+}
+</script>
